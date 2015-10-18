86113b5244cb33126db4644a4d8dedac1b6a0d34
lolisa <lolisa@marisa.moe>
2015-10-18 09:18:15 -0700

    add partial macro search

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,15 @@
 #include "mainwindow.hpp"
 #include "puzzle.hpp"
 #include "../MO/mo.hpp"
+#include "../cpp_common/combinator.hpp"
 using namespace MO;
-auto learner =
+auto iddfs_learner =
         []( const puzzle & state, size_t i, const auto & change_to )
         {
             vector< Action > ret;
-            breadth_first_search< Action >(
+            iterative_deepening_depth_first_search< Action >(
                 state,
-                []( const auto &, auto out ) { return std::copy( all_action( ).begin( ), all_action( ).end( ), out ); },
+                all_action,
                 act,
                 [&]( const auto & st )
                 {
@@ -20,11 +21,111 @@ auto learner =
                 },
                 back_inserter( ret ) );
             return ret;
+        };//THIS WILL NOT WORK ON YOUR MACHINE! IT STILL EXISTS BECAUSE I WANT TO ABSTRACT MORE FROM IT, JUST DONT USE!
+template< typename ACTION, typename STATE_VAR, size_t NUM_VAR, typename ALL_ACTION, typename ACT_FUNC, typename INVERSE_FUNC >
+boost::optional< std::vector< ACTION > > PARTIAL_MATCH(
+        const array< STATE_VAR, NUM_VAR > & st, size_t i, const STATE_VAR & change_to,
+        ALL_ACTION all_action, ACT_FUNC act_func, INVERSE_FUNC inverse_func, size_t depth )
+{
+    size_t l = depth / 2, r = depth / 2 + depth % 2;
+    auto powerset = common::fix(
+        [&]( const auto & SELF, size_t i )
+        {
+            if ( i == 0 ) { return vector< pair< array< STATE_VAR, NUM_VAR >, vector< ACTION > > >{ make_pair( st, vector< ACTION >{ } ) }; }
+            else
+            {
+                auto psm1 = SELF( i  - 1 );
+                decltype( psm1 ) ret;
+                ret.reserve( psm1.size( ) );
+                for ( const auto & pse : psm1 )
+                {
+                    all_action(
+                        pse.first,
+                        boost::make_function_output_iterator(
+                            [&]( const ACTION & act )
+                            {
+                                auto tem = pse.second;
+                                tem.push_back( act );
+                                ret.push_back( { act_func( pse.first, act ), tem } );
+                            } ) );
+                }
+                return ret;
+            }
+        } );
+    multimap< vector< STATE_VAR >, vector< Action > > change_invarance_l, change_invarance_r;
+    auto psl = powerset( l );
+    auto psr = powerset( r );
+    auto invariance_state =
+            [&]( const array< STATE_VAR, NUM_VAR > & arr )
+            {
+                std::vector< STATE_VAR > ret;
+                copy_n( arr.begin( ), i, back_inserter( ret ) );
+                return ret;
+            };
+    for ( const auto & e : psl )
+    { change_invarance_l.insert( { invariance_state( e.first ), e.second } ); }
+    for ( const auto & e : psr )
+    { change_invarance_r.insert( { invariance_state( e.first ), e.second } ); }
+    for ( const auto & el : change_invarance_l )
+    {
+        auto r = change_invarance_r.equal_range( el.first );
+        for ( const auto & er : common::make_range_container_proxy( r.first, r.second ) )
+        {
+            vector< Action > action1 = el.second, action2 = er.second;
+            for ( bool b = true; b; b = ! b )
+            {
+                auto new_state =
+                        accumulate(
+                            boost::make_transform_iterator( action2.rbegin( ), inverse_func ),
+                            boost::make_transform_iterator( action2.rend( ), inverse_func ),
+                            accumulate( action1.begin( ),
+                                        action1.end( ),
+                                        st,
+                                        act_func ),
+                            act_func );
+                if ( new_state[i] == change_to && invariance_state( new_state ) == invariance_state( st ) )
+                {
+                    action1.reserve( action1.size( ) + action2.size( ) );
+                    copy( boost::make_transform_iterator( action2.rbegin( ), inverse_func ),
+                          boost::make_transform_iterator( action2.rend( ), inverse_func ),
+                          back_inserter( action1 ) );
+                    return action1;
+                }
+                else { swap( action1, action2 ); }
+            }
+        }
+    }
+    return boost::optional< std::vector< ACTION > >( );
+}
+auto lambda_all_action = []( const puzzle & p, auto out ) { return all_action( p, out ); };
+auto learner =
+        []( const puzzle & state, size_t i, const auto & change_to )
+        {
+            for (size_t depth = 0; true; ++depth)
+            {
+                auto res = PARTIAL_MATCH< Action >(
+                            state,
+                            i,
+                            change_to,
+                            lambda_all_action,
+                            act,
+                            []( Action a )
+                            {
+                                return
+                                    a == Action::up ? Action::down :
+                                    a == Action::down ? Action::up :
+                                    a == Action::left ? Action::right :
+                                                        Action::left;
+                            },
+                            depth );
+                if ( res ) { return * res; }
+            }
         };
 auto lambda_act = []( const puzzle & p, Action a ) { return act( p, a ); };
 struct impl
 {
-    MO< size_t, 16, Action, decltype( lambda_act ), decltype( learner ) > mo = make_MO( goal_state, all_action( ), lambda_act, learner );
+    MO< size_t, 16, Action, decltype( lambda_all_action ), decltype( lambda_act ), decltype( learner ) > mo =
+            make_MO< Action >( goal_state, lambda_all_action, lambda_act, learner );
     puzzle p;
     explicit impl( const puzzle & p ) : p( p ) { }
 };
@@ -51,13 +152,10 @@ void MainWindow::on_pushButton_3_clicked()
 
 void MainWindow::on_pushButton_4_clicked()
 {
-    std::vector< std::vector< Action > > res;
-    p_impl->mo.solve( p_impl->p, std::back_inserter( res ) );
-    auto it = std::find_if_not( res.begin(), res.end(), []( const auto& t ){ return t.empty(); });
-    if ( it != res.end() ) {
-        p_impl->p = p_impl->mo.apply_macro( p_impl->p, it->begin( ), it->end( ) );
-        repaint();
-    }
+    std::vector< Action > res;
+    p_impl->mo.solve_step( p_impl->p, std::back_inserter( res ) );
+    p_impl->p = p_impl->mo.apply_macro( p_impl->p, res.begin( ), res.end( ) );
+    repaint();
 }
 
 void MainWindow::on_pushButton_clicked()
--- puzzle.hpp
+++ puzzle.hpp
@@ -11,50 +11,19 @@ std::random_device rd;
 
 enum class Action { up, down, left, right };
 
-std::vector< Action > & all_action( )
+template< typename OUTITER >
+OUTITER all_action( const puzzle &, OUTITER out )
 {
-    static std::vector< Action > act = { { Action::up, Action::down, Action::left, Action::right } };
-    return act;
+    * out = Action::up; ++out;
+    * out = Action::down; ++out;
+    * out = Action::left; ++out;
+    * out = Action::right; ++out;
+    return out;
 }
 
-puzzle act( const puzzle & state, Action a ) {
-    auto i = std::find(state.begin( ), state.end( ), 0) - state.begin( );
-    assert( i < 16 );
-    auto act = [&]( bool is_valid, size_t swap_with )
-    {
-        if ( is_valid )
-        {
-            puzzle ret = state;
-            std::swap( ret[i], ret[swap_with] );
-            return ret;
-        }
-        else { return state; }
-    };
-    switch ( a )
-    {
-        case Action::up : return act( i > 3, i - 4 );
-        case Action::down : return act( i < 12, i + 4 );
-        case Action::left : return act( i % 4 > 0, i - 1 );
-        case Action::right : return act( i % 4 < 3, i + 1 );
-    }
-}
-constexpr puzzle goal_state = { { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 } };
-
-Action random_action( ) {
-    switch (std::uniform_int_distribution<int> (1, 4)(rd)) {
-        case 1 : return Action::up;
-        case 2 : return Action::down;
-        case 3 : return Action::left;
-        default : return Action::right;
-    }
-}
-
-puzzle random_puzzle( ) {
-    std::vector<Action> moves;
-    for (int i = 0; i < 152; i++) {
-        moves.push_back( random_action() );
-    }
-    return std::accumulate(moves.begin(), moves.end(), goal_state, act);
+template< typename OS >
+OS & operator << ( OS & os, Action act ) {
+    return os << ( act == Action::up ? 'U' : act == Action::down ? 'D' : act == Action::left ? 'L' : 'R' );
 }
 
 template< typename OS >
@@ -66,6 +35,35 @@ OS & operator << ( OS & os, const puzzle & state ) {
         os << std::endl;
     }
     return os;
+}
+
+puzzle act( const puzzle & state, Action a ) {
+    auto i = std::find( state.begin( ), state.end( ), 0 ) - state.begin( );
+    assert( i < 16 );
+    puzzle ret = state;
+    std::swap(
+        ret[i],
+        ret[a == Action::up ? ( i > 3 ? i - 4 : i ) :
+            a == Action::down ? ( i < 12 ? i + 4 : i ) :
+            a == Action::left ? ( i % 4 > 0 ? i - 1 : i ) :
+                                ( i % 4 < 3 ? i + 1 : i )] );
+    return ret;
+}
+
+constexpr puzzle goal_state = { { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
+
+puzzle random_move( const puzzle & p ) {
+    std::vector< Action > acts;
+    all_action( p, std::back_inserter( acts ) );
+    return act( p, acts[ std::uniform_int_distribution< int >( 0, acts.size( ) - 1 )( rd ) ] );
+}
+
+puzzle random_puzzle( ) {
+    auto ret = goal_state;
+    for (int i = 0; i < 152; i++) {
+        ret = random_move( ret );
+    }
+    return ret;
 }
 #endif // PUZZLE_HPP
 
